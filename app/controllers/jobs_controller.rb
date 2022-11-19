edc8811fc2b2efe76aedc5ce917567ed11c47c90
Caio Felipe Motta Ladeia <caio.felipemotta@hotmail.com>
2022-11-19 01:33:24 -0300

    Creating job postings

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -27,11 +27,15 @@ class JobsController < ApplicationController
   def create
     @job = Job.new(job_params)
     @job.account = current_user.account
-
     if @job.save
-      redirect_to @job, notice: "Job was successfully created."
+      html = render_to_string(partial: 'job', locals: { job: @job })
+      render operations: cable_car
+                           .prepend('#jobs', html: html)
+                           .dispatch_event(name: 'submit:success')
     else
-      render :new, status: :unprocessable_entity
+      html = render_to_string(partial: 'form', locals: { job: @job })
+      render operations: cable_car
+                           .inner_html('#job-form', html: html), status: :unprocessable_entity
     end
   end
 
@@ -65,7 +69,7 @@ class JobsController < ApplicationController
     end
 
     # Only allow a list of trusted parameters through.
-    def job_params
-      params.require(:job).permit(:title, :status, :job_type, :location, :account_id)
-    end
+  def job_params
+    params.require(:job).permit(:title, :status, :job_type, :location, :account_id, :description)
+  end
 end
