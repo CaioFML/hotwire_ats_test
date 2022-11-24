af4dbc79edc76a521ab214233617baef983b40cd
Caio Felipe Motta Ladeia <caio.felipemotta@hotmail.com>
2022-11-23 22:21:20 -0300

    Clean up applicant group queries

--- app/controllers/applicants_controller.rb
+++ app/controllers/applicants_controller.rb
@@ -6,7 +6,9 @@ class ApplicantsController < ApplicationController
   include Filterable
 
   def index
-    @applicants = filter!(Applicant).for_account(current_user.account_id)
+    @grouped_applicants = filter!(Applicant)
+                            .for_account(current_user.account_id)
+                            .group_by(&:stage)
   end
 
   # GET /applicants/1 or /applicants/1.json
